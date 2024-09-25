eb822e97b04726b28409d7d1f86d28a8509c25ad
chekmarev <chekmarev@arrival.com>
2024-09-25 15:30:38 +0400

    Fix part2

--- day1/part2.py
+++ day1/part2.py
@@ -2,18 +2,23 @@ with open("input.txt", 'r') as file:
     lines = [line.rstrip('\n') for line in file.readlines()]
 digits = list()
 replacements = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+def replace_words_from_right(line):
+    i = len(line)
+    while i > 0:
+        for word, digit in replacements.items():
+            if line[i - len(word):i] == word:
+                line = line[:i - 1] + digit + line[i:]
+                i -= 1
+                break
+        else:
+            i -= 1
+    return line
+
+
 for line in lines:
     original_line = line
-    i = 3
-    while i <= len(line):
-        str_temp = line[0:i]
-        for key, value in replacements.items():
-            if key in str_temp:
-                str_temp = str_temp.replace(key, value)
-                line = str_temp + line[i:]
-                i = 2
-                break
-        i += 1
+    line = replace_words_from_right(line)
 
     digit_str = ''.join(filter(lambda x: x.isdigit(), line))
     if len(digit_str) == 1:
