fa4fcb1f806b9d7eafbc08a9e3c2fa70ac27159a
chekmarev <chekmarev@arrival.com>
2024-10-03 11:28:28 +0400

    fix solution part 2 day 4

--- day4/part2.py
+++ day4/part2.py
@@ -2,8 +2,10 @@ import re
 
 with open("input.txt", 'r') as file:
     cards = list([line.rstrip('\n') for line in file.readlines()])
-summ = 0
+games = dict()
+gameNo = 0
 for card in cards:
+    gameNo += 1
     numbers = card.split(":")[1]
     win_numbers = numbers.split("|")[0]
     my_numbers = numbers.split("|")[1]
@@ -19,10 +21,26 @@ for card in cards:
     for match in matches:
         my_set.add(int(match.group(1)))
 
-    score = 0
+    summ = 0
     for number in win_set:
         if number in my_set:
-            score = 1 if score == 0 else score * 2
-    summ += score
+            summ += 1
+    games[gameNo] = summ
 
-print(summ)
+my_cards = list(range(1,gameNo+1,1))
+n_cards = gameNo
+while (True):
+    new_cards = list()
+    for card in my_cards:
+        new_cards_count = games[card]
+        for i in range(1, new_cards_count + 1, 1):
+            new_cards_no = card + i
+            if new_cards_no <= gameNo:
+                new_cards.append(new_cards_no)
+                n_cards += 1
+    if len(my_cards) == 0:
+        break
+    else:
+        my_cards = new_cards
+
+print(n_cards)
